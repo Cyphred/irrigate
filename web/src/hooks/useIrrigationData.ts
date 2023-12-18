e0fe8e04739b7e81cba6773dc5a1bb728fadf4db
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-12-18 16:11:06 +0800

    Further simplifies irrigation data fetchers

--- web/src/hooks/useIrrigationData.ts
+++ web/src/hooks/useIrrigationData.ts
@@ -1,24 +1,48 @@
-import useRequest from "./useRequest";
+import axios from "axios";
 import StateData from "../types/StateData";
+import { useState } from "react";
 
 export default () => {
-  const { isLoading, error, get, patch } = useRequest();
+  const [isLoading, setIsLoading] = useState(false);
 
   const getIrrigationData = async () => {
-    const data = await get(`${import.meta.env.VITE_API_URI}/sensors/1`);
-    return data as StateData;
+    setIsLoading(true);
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URI}/sensors/1`
+      );
+      setIsLoading(false);
+
+      if (response.status >= 400) {
+        return;
+      }
+
+      return response.data as StateData;
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   const setIrrigationState = async (gateOpen: boolean) => {
-    const data = await patch(`${import.meta.env.VITE_API_URI}/sensors/1`, {
-      gateOpen,
-    });
-    return data as StateData;
+    try {
+      const response = await axios.patch(
+        `${import.meta.env.VITE_API_URI}/sensors/1`,
+        { gateOpen }
+      );
+      setIsLoading(false);
+
+      if (response.status >= 400) {
+        return;
+      }
+
+      return response.data as StateData;
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return {
     isLoading,
-    error,
     getIrrigationData,
     setIrrigationState,
   };
